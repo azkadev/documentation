0f53d3fb49cd4d5fc3a5c67c93833a0936f89479
￴￴ <>
2024-07-20 19:30:04 +0700

    up

--- package/documentation/lib/widget/markdown/images_node.dart
+++ package/documentation/lib/widget/markdown/images_node.dart
@@ -203,14 +203,14 @@ class _ImagesWidgetState extends State<MediasWidget> {
               style: TextStyle(
                 color: context.theme.indicatorColor,
                 fontSize: 20,
-                          shadows: [
-                            BoxShadow(
-                              color: context.theme.shadowColor.withAlpha(110),
-                              spreadRadius: 1,
-                              blurRadius: 7,
-                              offset: const Offset(0, 3), // changes position of shadow
-                            ),
-                          ],
+                shadows: [
+                  BoxShadow(
+                    color: context.theme.shadowColor.withAlpha(110),
+                    spreadRadius: 1,
+                    blurRadius: 7,
+                    offset: const Offset(0, 3), // changes position of shadow
+                  ),
+                ],
               ),
             ),
             image ?? const SizedBox.shrink(),
@@ -344,15 +344,14 @@ class FullScreenPageState extends State<FullScreenPage> {
                   Icons.arrow_back,
                   color: widget.dark ? Colors.white : Colors.black,
                   size: 25,
-
-                          shadows: [
-                            BoxShadow(
-                              color: context.theme.shadowColor.withAlpha(110),
-                              spreadRadius: 1,
-                              blurRadius: 7,
-                              offset: const Offset(0, 3), // changes position of shadow
-                            ),
-                          ],
+                  shadows: [
+                    BoxShadow(
+                      color: context.theme.shadowColor.withAlpha(110),
+                      spreadRadius: 1,
+                      blurRadius: 7,
+                      offset: const Offset(0, 3), // changes position of shadow
+                    ),
+                  ],
                 ),
               ),
             ),
